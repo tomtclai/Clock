a718ffc364cfc5df1f33111a3592f0270af50c6f
Tom Lai <tomtclai@gmail.com>
2016-03-19 11:19:56 -0700

    init

---
+++ Clock/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Tom Lai on 3/19/16.
+//  Copyright © 2016 Lai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var clockLabel: UILabel!
+    override func viewDidLoad() {
+        updateClockLabel();
+        NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "updateClockLabel", userInfo: nil, repeats: true)
+        self.performSelector("updateClockLabel", withObject: nil, afterDelay: 1)
+    }
+    func updateClockLabel() {
+        let date = NSDate()
+        let timeStr = NSDateFormatter.localizedStringFromDate(date, dateStyle: .NoStyle, timeStyle: .ShortStyle)
+        clockLabel.text = timeStr
+        print("1");
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
